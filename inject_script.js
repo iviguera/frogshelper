1dab2f190fbc86e96f0f400d5cd37b798feb5414
Ivan <ivanviguera@gmail.com>
2017-04-08 18:25:18 +0200

    Optimizing iteration and removing extra code.

--- inject_script.js
+++ inject_script.js
@@ -1,27 +1,10 @@
+
 var contracts_table = document.getElementById("form").getElementsByClassName("table")[0];
 var r = 0;
 while(row = contracts_table.rows[r])
 {
-	var destination_cell = 0;
-	if(window.location.search != "") {
-		var filters = window.location.search.replace("?", "").split("&");
-		var f = 0;
-		// 2 iterations: start and end filters => ?start=Jita&end=&sort=age
-		var max_iterations = 2;
-		while(f < max_iterations) {
-			var values = filters[f].split("=");
-			if(values[1] != '') {
-				destination_cell++;
-			}
-			f++;
-		}
-		//fix Dead Jumps column
-		if(destination_cell == 2) {
-			destination_cell++;
-		}
-	}
-	var cell = row.cells[destination_cell];
-	if(cell.innerText.includes("Citadel")) {
+	var row_html = row.innerHTML;
+	if(row_html.includes("Citadel") || row_html.includes("No collateral")) {
 		row.style.display = "none";
 	}
 	r++;
